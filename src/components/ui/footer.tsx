debde596cfc8d25ced161d6a65c9c86af91e79c9
matteoo.eth <mateogalic112@gmail.com>
2024-02-25 11:06:12 +0100

    Update footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -16,9 +16,7 @@ const Footer = () => {
         <div className="w-full h-[1px] bg-slate-300 opacity-20" />
         <div className="flex w-full flex-col items-center justify-center  pb-4 md:flex-row md:justify-between">
           <p className="text-sm text-slate-400 mb-4 md:mb-0">
-            &copy; {currentYear}{" "}
-            <a href="https://material-tailwind.com/">Alpha Code d.o.o.</a>. All
-            Rights Reserved.
+            &copy; {currentYear} Alpha Code d.o.o. All Rights Reserved.
           </p>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
             <Link
