b4dd10efc97a85dd697313237e85b0f2a58c8d15
matteoo.eth <mateogalic112@gmail.com>
2024-02-29 09:42:18 +0100

    Update cards

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -48,7 +48,7 @@ export const CardContainer = ({
     <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
       <div
         className={cn(
-          "py-20 flex items-center justify-center",
+          "my-20 lg:my-48 flex items-center justify-center",
           containerClassName
         )}
         style={{
--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import ServiceCard from "../cards/ServiceCard";
 import { ServiceType } from "../utils/services";
-import useIntersectionObserver from "../hooks/useIntersectionObserver";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 export const StickyScroll = ({ services }: { services: ServiceType[] }) => {
   const [activeCard, setActiveCard] = useState(0);
@@ -18,7 +17,7 @@ export const StickyScroll = ({ services }: { services: ServiceType[] }) => {
       animate={{
         backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
-      className="flex gap-16 justify-center relativespace-x-10 rounded-md pt-20 md:p-36"
+      className="flex gap-16 justify-center relative space-x-10 rounded-md pt-20 md:p-36"
     >
       <div className="relative flex items-start px-4">
         <div className="max-w-2xl">
@@ -32,25 +31,27 @@ export const StickyScroll = ({ services }: { services: ServiceType[] }) => {
           ))}
         </div>
       </div>
-      <motion.div className="hidden lg:block h-60 grow max-w-[40rem] min-w-[24rem] rounded-md sticky top-0 translate-y-[20%]  overflow-hidden">
-        <div className="relative h-96 w-full rounded-xl bg-slate-800 shadow-xl">
-          <div className="flex w-full gap-1.5 rounded-t-xl bg-slate-900 p-3">
-            <div className="h-3 w-3 rounded-full bg-red-500" />
-            <div className="h-3 w-3 rounded-full bg-yellow-500" />
-            <div className="h-3 w-3 rounded-full bg-green-500" />
+      <div className="my-40">
+        <motion.div className="hidden lg:block h-60 grow max-w-[40rem] min-w-[24rem] rounded-md sticky top-[50%] translate-y-[-50%]  overflow-hidden">
+          <div className="relative h-96 w-full rounded-xl bg-slate-800 shadow-xl">
+            <div className="flex w-full gap-1.5 rounded-t-xl bg-slate-900 p-3">
+              <div className="h-3 w-3 rounded-full bg-red-500" />
+              <div className="h-3 w-3 rounded-full bg-yellow-500" />
+              <div className="h-3 w-3 rounded-full bg-green-500" />
+            </div>
+            <div className="p-2 relative z-10">
+              <p className="font-mono text-lg text-slate-200">
+                <span className="text-green-300">~</span>{" "}
+                {services[activeCard].sideTitle}
+              </p>
+            </div>
           </div>
-          <div className="p-2 relative z-10">
-            <p className="font-mono text-lg text-slate-200">
-              <span className="text-green-300">~</span>{" "}
-              {services[activeCard].sideTitle}
-            </p>
-          </div>
-        </div>
 
-        <span className="absolute left-[50%] bottom-[15%] -translate-x-[50%]  text-slate-700">
-          <Icon className="w-20 h-20" />
-        </span>
-      </motion.div>
+          <span className="absolute left-[50%] bottom-[15%] -translate-x-[50%]  text-slate-700">
+            <Icon className="w-20 h-20" />
+          </span>
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
@@ -64,13 +65,14 @@ const Card = ({
   index: number;
   setActiveCard: Dispatch<SetStateAction<number>>;
 }) => {
-  const { containerRef, inView } = useIntersectionObserver();
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { amount: 1 });
 
   useEffect(() => {
-    if (inView) {
+    if (isInView) {
       setActiveCard(index);
     }
-  }, [inView, index, setActiveCard]);
+  }, [isInView, index, setActiveCard]);
 
   return (
     <motion.div
@@ -78,11 +80,11 @@ const Card = ({
         opacity: 0,
       }}
       animate={{
-        opacity: inView ? 1 : 0.8,
+        opacity: isInView ? 1 : 0.8,
       }}
-      ref={containerRef}
+      ref={ref}
       key={item.title}
-      className="lg:h-[100vh]"
+      className="max-h-[100vh]"
     >
       <ServiceCard
         title={item.title}
