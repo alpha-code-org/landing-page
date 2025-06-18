48599d93e533a920d99de24c7ed85185865d0cab
Mateo Galic <mateogalic112@gmail.com>
2025-06-18 20:35:08 +0200

    add mdx remote to transpilePackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import createMDX from '@next/mdx'
-
+import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,6 +11,7 @@ const nextConfig = {
       { hostname: "source.unsplash.com" },
     ],
   },
+  transpilePackages: ["next-mdx-remote"],
 };
 
 const withMDX = createMDX({
